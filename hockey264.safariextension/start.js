b850706012a7ea41adcdf00981f628c97de84cac
Paul Grave <paul@stomer.net>
2011-12-30 21:00:05 +0000

    wip: carousel option

--- hockey264.safariextension/start.js
+++ hockey264.safariextension/start.js
@@ -1,40 +1,49 @@
 if( window.top == window ) {
-
-	// no need to run this on the video page
+	
 	var re = /video\.nhl\.com/;
-	if( !re.test(document.URL) ) {
-		// the end script gets called after $(document).ready so I can't intercept the function that gets called.
-		// so what I do instead is setup some listeners for node removal/insertion and intercept the insertion of 
-		// the flash crap and remove of the nice html and revert the changes.
+	safari.self.tab.dispatchMessage("isHTMLCarouselEnabled",1);
 	
-		// mbvHome contains the nice HTML version that we want on the page, not the crappy flash that replaces it.
-		// I need a reference to it so that when the flash is added, I can remove the flash and append mbvHome
-		var mbvHome;
-
-		document.addEventListener('DOMNodeRemoved', function(event) {
-			const element = event.target;
-			if( element.id == 'flashOn') {
-				mbvHome = element.parentNode.cloneNode(true);
+	safari.self.addEventListener("message", function(theMessageEvent) {
+				
+		if (theMessageEvent.name === "HTMLCarouselIsEnabled") {
+		
+			// no need to run this on the video page
+			if( !re.test(document.URL) ) {
+				// the end script gets called after $(document).ready so I can't intercept the function that gets called.
+				// so what I do instead is setup some listeners for node removal/insertion and intercept the insertion of 
+				// the flash crap and remove of the nice html and revert the changes.
+			
+				// mbvHome contains the nice HTML version that we want on the page, not the crappy flash that replaces it.
+				// I need a reference to it so that when the flash is added, I can remove the flash and append mbvHome
+				var mbvHome;
+		
+				document.addEventListener('DOMNodeRemoved', function(event) {
+					const element = event.target;
+					if( element.id == 'flashOn') {
+						mbvHome = element.parentNode.cloneNode(true);
+					}
+				} ,false);
+			
+				document.addEventListener('DOMNodeInserted', function(event) {
+				
+					const element = event.target
+					if(element instanceof HTMLObjectElement && element.id == 'nhl') {
+						var parentNode = element.parentNode;
+		
+						if( parentNode && mbvHome ) {
+							parentNode.removeChild(element);
+							parentNode.appendChild(mbvHome);
+							document.getElementById('flashOff').style.display = '';
+							document.getElementById('flashOn').style.display = 'none';
+						}
+		
+					} 
+				
+				} ,true);		
 			}
-		} ,false);
+		}
 	
-		document.addEventListener('DOMNodeInserted', function(event) {
+	}, false);	
 		
-			const element = event.target
-			if(element instanceof HTMLObjectElement && element.id == 'nhl') {
-				var parentNode = element.parentNode;
-
-				if( parentNode && mbvHome ) {
-					parentNode.removeChild(element);
-					parentNode.appendChild(mbvHome);
-					document.getElementById('flashOff').style.display = '';
-					document.getElementById('flashOn').style.display = 'none';
-				}
-
-			} 
-		
-		} ,true);		
-	}
-	
 
 }
